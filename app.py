f87e91ddb2cb27e8c411667d3fc7e5916868acde
y-fujii <y-fujii@kdl.co.jp>
2020-04-27 16:17:15 +0900

    edit app.py

--- app.py
+++ app.py
@@ -240,5 +240,10 @@ def main():
 @app.route('/hello')
 def hello_world():
     return jsonify({'message': "Hello, world"})
+
+@app.route('/body')
+def body():
+    return jsonify({‘b_tmp’: 37.5, ‘status’: true)
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="8000")
